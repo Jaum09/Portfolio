b98476c15cc6a37eb70d5ebf8261926a36a66ea1
Jaum09 <joao.silva62@estudante.ifms.edu.br>
2023-09-19 07:37:47 -0400

    Port atualizado

--- src/componentes/header/Header.tsx
+++ src/componentes/header/Header.tsx
@@ -1,12 +1,13 @@
 import './Header.css'
 import logo from '../../assets/Portfolio.png'
+import wall from '../../assets/WallpaperHeader.png'
 
 function Header(){
     return(
         <header className='header-container'>
             <div className='header'>
-                <img src={logo} className='logo'></img>
-            </div>
+                <img src={logo} className='logo'></img>    
+            </div><img src={wall} className='Wallpaper'></img>
         </header>
     )
 }
